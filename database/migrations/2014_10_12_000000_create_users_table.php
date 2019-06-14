1ab59393138cf46280bca6af867c2cec5c865c99
Ebuka Ubah <ebukaubah78@gmail.com>
2019-06-14 20:56:17 +0100

    initializing ponzi application

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('username',190)->unique();
+            $table->string('password');
+            $table->string('image')->nullable();
+            $table->string('bankName')->nullable();
+            $table->string('accountName')->nullable();
+            $table->string('accountNumber')->nullable();
+            $table->integer('potential')->nullable();
+            $table->boolean('blocked');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
